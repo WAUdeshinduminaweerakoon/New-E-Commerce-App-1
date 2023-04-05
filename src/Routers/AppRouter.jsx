96e3d03b191eb0656a9af8288aa163c26a78ec01
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-04-06 01:36:55 +0530

    changes in files

--- src/Routers/AppRouter.jsx
+++ src/Routers/AppRouter.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import MainLayout from "../Layout/MainLayout"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import User from "../Pages/User/User";
 
 
 
@@ -9,14 +11,14 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<MainLayout/>}> 
 
-         <Route index element={<Body/>}  />
-
+         <Route index element={<Home/>} />
+         <Route path= "user" element={<User/>} />
     
         </Route>
       </Routes>
     
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
