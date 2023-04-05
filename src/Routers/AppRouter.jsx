6094db95e24ea144973631f2dbb94973ad4e77d9
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-04-06 03:21:00 +0530

    Protectected routing new vasion

--- src/Routers/AppRouter.jsx
+++ src/Routers/AppRouter.jsx
@@ -1,26 +1,58 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from "react-router-dom";
+import {  RouterProvider,  createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import User from "../Pages/User/User";
 import Login from "../Pages/Login/Login";
 import UserProtected from "./UserProtected";
 
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <MainLayout/>,
+        children : [
+            {
+                index : true,
+                element:<Home/>
+            },
+            {
+                element : <UserProtected/>,
+                children:[
+                    {
+                        path:'user',
+                        element : <User/>
 
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        path:"/login",
+        element:<Login/>,
+    },
+]);
 
 const AppRouter = () => {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<MainLayout/>}> 
-                <Route index element={<Home/>}/>
-                <Route element={<UserProtected/>}>
-                    <Route path= "user" element={<User/>}/>
-                </Route>
-            </Route>
-            <Route path="/login"element={<Login/>}/>
-        </Routes>
-    </BrowserRouter>
-  );
-};
+    return <RouterProvider router  ={router}/>
+}
+
+
+
+// const AppRouter = () => {
+//   return (
+//     <BrowserRouter>
+//         <Routes>
+//             <Route path="/" element={<MainLayout/>}> 
+//                 <Route index element={<Home/>}/>
+//                 <Route element={<UserProtected/>}>
+//                     <Route path= "user" element={<User/>}/>
+//                 </Route>
+//             </Route>
+//             <Route path="/login"element={<Login/>}/>
+//         </Routes>
+//     </BrowserRouter>
+//   );
+// };
 
 export default AppRouter;
