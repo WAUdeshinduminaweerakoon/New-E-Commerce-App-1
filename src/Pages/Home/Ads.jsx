ce038cda13538bbdb5706fdb6a7cdc19a0576f76
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-04-06 16:13:37 +0530

    connected ads(photo) to the ads.js

---
+++ src/Pages/Home/Ads.jsx
@@ -0,0 +1,64 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay} from "swiper";
+// Import Swiper styles
+import "swiper/css";
+
+const adsElement=[
+     {
+        imageUrl:"https://wallpapers.com/images/featured/ztbad1qj8vdjqe0p.jpg"
+     },
+     {
+        imageUrl:"https://cdn.pixabay.com/photo/2017/10/10/07/48/hills-2836301__340.jpg"
+     },
+     {
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/a/a2/1121098-pink-nature-wallpaper-1920x1080-lockscreen.jpg"
+     },
+     {
+        imageUrl:"https://c4.wallpaperflare.com/wallpaper/388/726/456/laptop-backgrounds-nature-images-1920x1200-wallpaper-preview.jpg"
+     },
+
+]
+
+
+
+const Ads = () => {
+  return (
+    <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay]}
+       className="rounded-lg overflow-hidden"
+      >
+       {adsElement.map(({imageUrl},index)=>(
+           <SwiperSlide key={index}>
+               <AdUnit imageUrl={imageUrl} id={index}/>
+           </SwiperSlide>
+            ))} 
+
+     
+      </Swiper>
+  )
+}
+
+export default Ads;
+
+//https://wallpapers.com/images/featured/ztbad1qj8vdjqe0p.jpg
+//https://upload.wikimedia.org/wikipedia/commons/a/a2/1121098-pink-nature-wallpaper-1920x1080-lockscreen.jpg
+
+const AdUnit = ({imageUrl,id})=>(
+
+    <img 
+    src = {imageUrl}
+    alt={`ad${id}`}
+    className="w-full ogject-contain rounded-lg"
+    />
+
+);
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
+import Ads from "./Ads";
 
 
 const Home = () => {
   return (
     <div className="px-5 py-[100px] w-full h-screen overflow-y-scroll">
-        Home
+        <Ads/>
     </div>
   );
 };
 
-export default Home
+export default Home;
