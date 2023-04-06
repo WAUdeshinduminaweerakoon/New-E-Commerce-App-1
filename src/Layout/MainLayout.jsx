0a5f47d30a3b78f8582d292f3cd04a9486f14006
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-04-06 11:20:11 +0530

    added home butten in footer

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -5,7 +5,7 @@ import Header from "../components/Header/Header";
 
 const MainLayout = () => {
   return (
-    <div className="w-full h-screen ">
+    <div className=" over   w-screen h-screen ">
         <Header/>
         <Outlet/>
         <Footer/>
