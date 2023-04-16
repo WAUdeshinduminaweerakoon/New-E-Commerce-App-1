d36ee6572178715131a8497c4701bed7cfb7aecf
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-04-16 16:21:39 +0530

    added the main category section

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -3,18 +3,25 @@
 const Category = () => {
   return (
     <div className="px-5 py-[100px] w-full h-screen overflow-y-scroll">
-        <div style={{
-        boxShadow:"rgba(30, 8, 108, 0.24) 0px 3px 8px",
-    }} className="w-full p-2 grid grid-cols-3 grid-flow-row-[auto] gap-5 ">
-        <CategoryItems/>
-        <CategoryItems/>
-        <CategoryItems/>
-        <CategoryItems/>
-        <CategoryItems/>
-        <CategoryItems/>
+            <section
+             style={{
+            boxShadow:"rgba(30, 8, 108, 0.24) 0px 3px 8px",
+        }} className="w-full p-2 ">
+            <h1 className="text-lg font-bold mb-3 mt-2 ml-2 ">Main Categories</h1>
+                    <div className="grid grid-cols-3 grid-flow-row-[auto] gap-5">
 
-        </div>
-        
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    <CategoryItems/>
+                    
+                    </div>
+            </section>
         Category
     </div>
   );
@@ -23,6 +30,7 @@ const Category = () => {
 const CategoryItems = ()=><div className="w-full rounded-md border -2 border-pink-600 flex flex-col items-center">
     <img src="https://wallpapers.com/images/featured/autumn-kquin5jg1dtbyxgc.jpg" alt="categoryitem"/>
     <h2>Category</h2>
+    <h2>Name</h2>
 
 </div>
 
