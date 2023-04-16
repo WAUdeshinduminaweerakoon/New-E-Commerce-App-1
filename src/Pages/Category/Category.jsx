736d4632c415a402f0db265e0b07855ac31b7649
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-04-16 21:40:09 +0530

    added the Category arr

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,6 +1,30 @@
 import { IconButton } from "@mui/material";
 
 
+const categoryArr = [
+    {
+        imageUrl : 'https://wallpapers.com/images/featured/autumn-kquin5jg1dtbyxgc.jpg',
+        title : 'Category Title 1'
+    },
+    {
+        imageUrl : 'https://wallpapers.com/images/featured/autumn-kquin5jg1dtbyxgc.jpg',
+        title : 'Category Title 2'
+    },
+    {
+        imageUrl : 'https://wallpapers.com/images/featured/autumn-kquin5jg1dtbyxgc.jpg',
+        title : 'Category Title 3'
+    },
+    {
+        imageUrl : 'https://wallpapers.com/images/featured/autumn-kquin5jg1dtbyxgc.jpg',
+        title : 'Category Title 4'
+    },
+    {
+        imageUrl : 'https://wallpapers.com/images/featured/autumn-kquin5jg1dtbyxgc.jpg',
+        title : 'Category Title 5'
+    },
+]
+
+
 const Category = () => {
   return (
     
@@ -11,16 +35,8 @@ const Category = () => {
         }} className="w-full p-2 ">
             <h1 className="text-lg font-bold mb-3 mt-2 ml-2 ">Main Categories</h1>
                     <div className="grid grid-cols-3 grid-flow-row-[auto] gap-5">
+                        {categoryArr.map(({imageUrl,title},index)=><CategoryItems key= {index} imageUrl={imageUrl} title={title}/>)}
 
-                    <CategoryItems/>
-                    <CategoryItems/>
-                    <CategoryItems/>
-                    <CategoryItems/>
-                    <CategoryItems/>
-                    <CategoryItems/>
-                    <CategoryItems/>
-                    <CategoryItems/>
-                    <CategoryItems/>
                     
                     </div>
             </section>
@@ -29,7 +45,9 @@ const Category = () => {
   );
 };
 
-const CategoryItems = ()=>(
+export default Category;
+
+const CategoryItems = ({imageUrl,title})=>(
 <IconButton sx={{
         color:'gray',
         padding:'1px',
@@ -40,14 +58,12 @@ const CategoryItems = ()=>(
   style={{
     boxShadow:"rgba(30, 8, 108, 0.24) 0px 3px 8px",
 }}
-className="m-1 w-full p-6 rounded-md  flex flex-col items-center">
-    <img src="https://wallpapers.com/images/featured/autumn-kquin5jg1dtbyxgc.jpg" alt="categoryitem"
+className="m-1 w-full py-6 px-3 mt-3 rounded-md  flex flex-col items-center">
+    <img src={imageUrl} alt={title}
     className=" rounded-full w-[50px] h-[50px]"/>
-    <h2 className=" text-[10px] text-black font-bold ">Category Name </h2>
+    <h2 className=" text-[10px] text-black font-bold ">{title} </h2>
     
 
 </div>
 </IconButton>
 );
-
-export default Category;
