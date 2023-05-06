62957fca9bd3dd53509600dc225544932ee49302
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-05-06 10:57:35 +0530

    added the initialState in categorySlice

--- src/Store/ReduxSlice/categorySlice.js
+++ src/Store/ReduxSlice/categorySlice.js
@@ -1,2 +1,11 @@
 import{createSlice} from "@reduxjs/toolkit"
 
+const initialState = [{
+    id:'category1',
+    title:''
+}]
+const categorySlice = createSlice({
+    name: 'categorySlice',
+    initialState,
+    reducers:{}
+})
