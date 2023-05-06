ceb8146eb49c9fea146db08aeb7e80d3ffd54b74
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-05-06 11:04:43 +0530

    added the reducer in categorySlice

--- src/Store/ReduxSlice/categorySlice.js
+++ src/Store/ReduxSlice/categorySlice.js
@@ -2,10 +2,29 @@ import{createSlice} from "@reduxjs/toolkit"
 
 const initialState = [{
     id:'category1',
-    title:''
-}]
+    title:'category title 1'
+},
+{
+    id:'category2',
+    title:'category title 2'
+},
+{
+    id:'category3',
+    title:'category title 3'
+},
+{
+    id:'category4',
+    title:'category title 4'
+},
+{
+    id:'category5',
+    title:'category title 5'
+},
+]
 const categorySlice = createSlice({
     name: 'categorySlice',
     initialState,
     reducers:{}
 })
+
+export default categorySlice.reducer;
